d61f1b2a29060e90447d9e6f7db5c5d2865091c0
Kumar Gaurav <139kumargaurav@gmail.com>
2025-09-20 00:02:09 +0530

    jwt and cookies authentication added

--- src/app.js
+++ src/app.js
@@ -2,13 +2,16 @@ const express = require("express");
 const dbConnect = require("./config/dbConnect");
 const User = require("./models/User");
 const { validateSignupData } = require("./utils/validationHelper");
+const jwt = require("jsonwebtoken");
 const apis = require("./utils/apis");
 const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.post(apis.SIGN_UP, async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
@@ -27,7 +30,9 @@ app.post(apis.SIGN_UP, async (req, res) => {
       throw new Error(error);
     }
   } catch (error) {
-    res.status(500).json({ message: "User signup failed", error: error.toString() });
+    res
+      .status(500)
+      .json({ message: "User signup failed", error: error.toString() });
   }
 });
 
@@ -35,14 +40,31 @@ app.post(apis.LOGIN, async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.findOne({ email });
-    if (!user || !(await bcrypt.compare(password, user.password))) {
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!user || !isValidPassword) {
       return res.status(401).json({ message: "Invalid email or password" });
     } else {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+      res.cookie("token", token,);
       res.status(200).json({ message: "Login successful", user });
     }
   } catch (error) {
     res.status(500).json({ message: "Login failed", error });
   }
+});
+
+app.get(apis.PROFILE, async (req, res) => {
+    try{
+        const cookie = req.cookies;
+        const decoded = jwt.verify(cookie.token, process.env.JWT_SECRET);
+        const user = await User.findById(decoded.id);
+        if(!user){
+            throw new Error("User not found");
+        }
+        res.send(`Welcome to your profile! ${user.firstName} ${user.lastName}` + user);
+    } catch (error) {
+        res.status(500).json({ message: "Profile retrieval failed", error });
+    }
 });
 
 app.get(apis.FEED, async (req, res) => {
--- src/utils/apis.js
+++ src/utils/apis.js
@@ -1,6 +1,7 @@
 const apis = {
     SIGN_UP: "/signup",
     LOGIN: "/login",
+    PROFILE: "/profile",
     FEED: "/feed",
     USER: "/user",
     UPDATE_USER: "/user/:id"
