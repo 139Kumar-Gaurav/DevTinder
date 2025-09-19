373f372d4a5dfd19aaeaa6d0719b1f95dfe383ec
Kumar Gaurav <139kumargaurav@gmail.com>
2025-09-19 16:01:37 +0530

    added api validations

--- src/app.js
+++ src/app.js
@@ -7,47 +7,92 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 
-app.post('/signup', async (req, res) => {
-    try {
-        await new User(req.body).save();
-        res.status(200).json({ message: "User signed up successfully" });
-    } catch (error) {
-        res.status(500).json({ message: "User signup failed", error });
-    }
+const validationMiddleware = (req, res, next) => {
+  const { firstName, lastName, email, password } = req.body;
+  if (firstName.length > 5 || lastName.length > 5 || !email || !password) {
+    return res
+      .status(400)
+      .json({
+        message:
+          "Validation failed. Please provide all required fields with valid data.",
+      });
+  }
+  next();
+};
+
+app.post("/signup", validationMiddleware, async (req, res) => {
+  const data = req.body;
+  try {
+    await new User(data).save();
+    res.status(200).json({ message: "User signed up successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "User signup failed", error });
+  }
 });
 
-app.get('/feed', async (req, res) => {
-    try {
-        const users = await User.find();
-        res.status(200).json(users);
-    } catch (error) {
-        res.status(500).json({ message: "User retrieval failed", error });
-    }
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: "User retrieval failed", error });
+  }
 });
 
-app.delete('/user', async (req, res)=>{
-    try{
-        const userToBedeleted = await User.findById(req.body.id);
-        if(!userToBedeleted){
-            return res.status(404).json({message: "User not found"});
-        }
-        await User.findByIdAndDelete(req.body.id);
-        res.status(200).json(`${userToBedeleted.firstName + ' ' + userToBedeleted.lastName} deleted successfully`);
-    } catch (error) {
-        res.status(500).json({ message: "User deletion failed", error });
+app.delete("/user", async (req, res) => {
+  try {
+    const userToBedeleted = await User.findById(req.body.id);
+    if (!userToBedeleted) {
+      return res.status(404).json({ message: "User not found" });
     }
+    await User.findByIdAndDelete(req.body.id);
+    res
+      .status(200)
+      .json(
+        `${
+          userToBedeleted.firstName + " " + userToBedeleted.lastName
+        } deleted successfully`
+      );
+  } catch (error) {
+    res.status(500).json({ message: "User deletion failed", error });
+  }
 });
 
-app.patch('/user', async (req, res)=>{
-    try{
-        const updatedUsers = await User.findByIdAndUpdate(req.body.id, req.body, { returnOriginal: false });
-        res.status(200).json({ message: "User updated successfully", user: updatedUsers });
-    } catch (error) {
-        res.status(500).json({ message: "User update failed", error });
+app.patch("/user/:id", async (req, res) => {
+  const userId = req.params.id;
+  const data = req.body;
+  const allowedUpdates = [
+    "firstName",
+    "lastName",
+    "password",
+    "age",
+    "imageUrl",
+    "about",
+    "skills",
+  ];
+  try {
+    const allowed = Object.keys(data).every((update) =>
+      allowedUpdates.includes(update)
+    );
+    if (!allowed) {
+      throw new Error("Invalid updates detected: ");
     }
-})
-
-
+    const updatedUsers = await User.findByIdAndUpdate(userId, data, {
+      returnOriginal: false,
+      runValidators: true,
+    });
+    if (!updatedUsers) {
+      throw new Error("User not found");
+    }
+    res
+      .status(200)
+      .json({ message: "User updated successfully", user: updatedUsers });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "User update failed", error: error.toString() });
+  }
+});
 
 dbConnect()
   .then(
--- src/models/User.js
+++ src/models/User.js
@@ -1,28 +1,60 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     firstName: {
-        type: String,
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 50,
     },
     lastName: {
-        type: String,
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 50,
     },
     email: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     age: {
-        type: Number,
+      type: Number,
+      min: 18,
     },
     gender: {
-        type: String,
-    }
-});
+      type: String,
+      required: true,
+      validate(value) {
+        if (
+          ["male", "female", "other"].includes(value.toLowerCase()) === false
+        ) {
+          throw new Error("Invalid gender");
+        }
+      },
+    },
+    imageUrl: {
+      type: String,
+      default: "https://sclpa.com/wp-content/uploads/2022/10/dummy-img-1.jpg",
+    },
+    about: {
+      type: String,
+      default: "I am a Software Engineer",
+    },
+    skills: {
+      type: [String],
+    },
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
