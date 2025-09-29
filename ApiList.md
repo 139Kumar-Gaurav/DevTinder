8b7ba00085d1ca91be2911a5e3af35424a82da4d
Kumar Gaurav <139kumargaurav@gmail.com>
2025-09-29 23:16:30 +0530

    added pagination into the feed api

--- ApiList.md
+++ ApiList.md
@@ -17,6 +17,6 @@
 ## userRouter
 - GET /user/requests/received
 - GET /user/connections
-- GET /user/feed - Gets you the profile of other users on the platform
+- GET /user/feed  or /user/feed?page=1&limit=10 - Gets you the profile of other users on the platform
 
 Status: ignored, inetrested, accepted, rejected
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,8 @@ const express = require("express");
 const apis = require("../utils/apis");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/ConnectionRequest");
+const User = require("../models/User");
+const { parse } = require("dotenv");
 const userRouter = express.Router();
 
 const CONNECTION_DATA = "firstName lastName age gender imageUrl about skills";
@@ -44,7 +46,9 @@ userRouter.get(apis.USER_CONNECTIONS, userAuth, async (req, res) => {
     }
 
     const userConnections = connectedUsers.map((connection) => {
-      if (connection.fromUserId._id.toString() === loggedInUser._id.toString()) {
+      if (
+        connection.fromUserId._id.toString() === loggedInUser._id.toString()
+      ) {
         return connection.toUserId;
       }
       return connection.fromUserId;
@@ -57,6 +61,46 @@ userRouter.get(apis.USER_CONNECTIONS, userAuth, async (req, res) => {
   } catch (error) {
     res.status(500).json({
       message: "Failed to fetch connections",
+      error: error.message.toString(),
+    });
+  }
+});
+
+userRouter.get(apis.USER_FEED, userAuth, async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+    const loggedInUser = req.user;
+    const connectedUsers = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    });
+    const userConnections = connectedUsers.map((connection) => {
+      if (
+        connection.fromUserId._id.toString() === loggedInUser._id.toString()
+      ) {
+        return connection.toUserId;
+      }
+      return connection.fromUserId;
+    });
+
+    const notConnectedUsers = await User.find({
+      _id: {
+        $nin: [...userConnections, loggedInUser._id].map((user) => user._id),
+      },
+    })
+      .select(CONNECTION_DATA)
+      .skip(skip)
+      .limit(limit);
+    res.status(200).json({
+      message: "Fetched all users successfully",
+      totalUsers: notConnectedUsers.length,
+      data: notConnectedUsers,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to fetch users",
       error: error.message.toString(),
     });
   }
