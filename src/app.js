c59217b4ac9e8541dfb9153e647b4be708470ee0
Kumar Gaurav <139kumargaurav@gmail.com>
2025-09-30 18:46:55 +0530

    handled cors issue for the frontend use

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const dbConnect = require("./config/dbConnect");
 const cookieParser = require("cookie-parser");
 const authRouter = require("./routes/auth");
@@ -9,6 +10,10 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 app.use(cookieParser());
 app.use(express.json());
 app.use("/", authRouter);
