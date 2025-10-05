675a142a1561f4172b0dbe34012880cebb61f03d
Kumar Gaurav <139kumargaurav@gmail.com>
2025-10-05 15:43:58 +0530

    reconfigured the db connect with the new creds

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,13 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
 
-const dbConnect = async() => await mongoose.connect(`mongodb+srv://139kumargaurav:DVyxoGYiOD7BJBZt@namastenodejs.vdyrcme.mongodb.net/devtinder`);
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://139kumargaurav:h50g5Q2F96EPz51q@namastenodejs.vdyrcme.mongodb.net/devtinder"
+    );
+  } catch (err) {
+    console.log("error in connecting ", err);
+  }
+};
 
 module.exports = dbConnect;
